d3dab33e49a071f933378f010583ff9cfcc4fc0e
Richard Newman <richardnewman@gogolabs.net>
2015-12-11 12:17:07 -0700

    ckpt-25

--- scripts/collection.js
+++ scripts/collection.js
@@ -3,9 +3,9 @@ var collectionItemTemplate =
   '  <img src="assets/images/album_covers/01.png"/>' +
   '  <div class="collection-album-info caption">' +
   '    <p>' +
-  '      <a class="album-name" href="/album.html"> The Colors </a>' +
+  '      <a class="album-name" href="album.html"> The Colors </a>' +
   '      <br/>' +
-  '      <a href="/album.html"> Pablo Picasso </a>' +
+  '      <a href="album.html"> Pablo Picasso </a>' +
   '      <br/>' +
   '      X songs' +
   '      <br/>' +
@@ -25,58 +25,3 @@ window.onload = function() {
     collectionContainer.innerHTML += collectionItemTemplate;
   }
 };
-
-/*
-<div class="collection-album-container column fourth">
-  <img src="assets/images/album_covers/01.png"/>
-  <div class="collection-album-info caption">
-    <p>
-      <a class="album-name" href="album.html">The Colors</a>
-      <br/>
-      <a href="album.html">Pablo Picasso</a>
-      <br/>
-      X songs
-      <br/>
-    </p>
-  </div>
-</div>
-<div class="collection-album-container column fourth">
-  <img src="assets/images/album_covers/01.png"/>
-  <div class="collection-album-info caption">
-  <p>
-    <a class="album-name" href="#">The Colors</a>
-    <br/>
-    <a href="#">Pablo Picasso</a>
-    <br/>
-    X songs
-    <br/>
-  </p>
-  </div>
-</div>
-<div class="collection-album-container column fourth">
-  <img src="assets/images/album_covers/01.png"/>
-  <div class="collection-album-info caption">
-  <p>
-    <a class="album-name" href="#">The Colors</a>
-    <br/>
-    <a href="#">Pablo Picasso</a>
-    <br/>
-    X songs
-    <br/>
-  </p>
-  </div>
-</div>
-<div class="collection-album-container column fourth">
-  <img src="assets/images/album_covers/01.png"/>
-  <div class="collection-album-info caption">
-    <p>
-      <a class="album-name" href="#">The Colors</a>
-      <br/>
-      <a href="#">Pablo Picasso</a>
-      <br/>
-      X songs
-      <br/>
-    </p>
-  </div>
-</div>
-*/
