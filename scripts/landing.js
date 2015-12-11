3f9e496560d8486ddb2da885895c3c03f4b3889c
Richard Newman <richardnewman@gogolabs.net>
2015-12-10 20:12:35 -0700

    ckpt-23-assign

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-  function revealPoint(point) {
-    points[point].style.opacity = 1;
-    points[point].style.transform = "scaleX(1) translateY(0)";
-    points[point].style.msTransform = "scaleX(1) translateY(0)";
-    points[point].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
+function revealPoint(point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
 
-  for (var point = 0; point < points.length; point++) {
-    revealPoint(point);
-  }
-};
+function animatePoints(points) {
+  forEach(points, revealPoint);
+}
 
 window.onload = function() {
   // Automatically animate the points on a tall screen where scrolling can't trigger the animation
@@ -20,6 +18,7 @@ window.onload = function() {
     animatePoints(pointsArray);
   }
 
+  // Automatically animate the points when the user scrolls down into that section
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var sellingDepth = 200;
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + sellingDepth;
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(array, callback) {
+  for (var index = 0; index < array.length; index++) {
+    callback(array[index]);
+  }
+}
