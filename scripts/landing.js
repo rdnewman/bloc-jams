02b1aa47bef70a9cb71e1fce11147a035ba77964
Richard Newman <richardnewman@gogolabs.net>
2015-12-10 20:01:11 -0700

    ckpt-23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
+var animatePoints = function(points) {
   function revealPoint(point) {
     points[point].style.opacity = 1;
     points[point].style.transform = "scaleX(1) translateY(0)";
@@ -11,4 +11,22 @@ var animatePoints = function() {
   for (var point = 0; point < points.length; point++) {
     revealPoint(point);
   }
+};
+
+window.onload = function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+  var contentHeight = 950;
+  if (window.innerHeight > contentHeight) {
+    animatePoints(pointsArray);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var sellingDepth = 200;
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + sellingDepth;
+
+  window.addEventListener("scroll", function(event) {
+    if (document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
 };
