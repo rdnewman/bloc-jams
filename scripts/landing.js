1efa98be2fbc2a8bbd175305226eb192890f9c90
Richard Newman <richardnewman@gogolabs.net>
2015-12-15 19:31:18 -0700

    ckpt-29

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,27 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-  function revealPoint(point) {
-    points[point].style.opacity = 1;
-    points[point].style.transform = "scaleX(1) translateY(0)";
-    points[point].style.msTransform = "scaleX(1) translateY(0)";
-    points[point].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
-
-  for (var point = 0; point < points.length; point++) {
-    revealPoint(point);
-  }
-};
-
-window.onload = function() {
-  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-  var contentHeight = 950;
-  if (window.innerHeight > contentHeight) {
-    animatePoints(pointsArray);
-  }
-
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var sellingDepth = 200;
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + sellingDepth;
-
-  window.addEventListener("scroll", function(event) {
-    if (document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
-    }
+var animatePoints = function() {
+  $.each($('.point'), function() {
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
   });
 };
+
+$(window).load( function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+  var contentHeight = 950;
+  if ($(window).height() > contentHeight) {
+    animatePoints();
+  }
+
+  var sellingDepth = 200;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + sellingDepth;
+
+  $(window).scroll( function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+        animatePoints();
+    }
+  });
+});
